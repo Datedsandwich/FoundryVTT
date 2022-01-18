392f37927ff589034f5ecc8a713d987342cd0e44
Luke Robinson <robinsonlm@live.co.uk>
2022-01-18 11:51:51 +0000

    Make Aid simpler

--- Spells/2nd Level/aid.js
+++ Spells/2nd Level/aid.js
@@ -1,28 +1,9 @@
 if (args[0].tag === "OnUse") {
     if (args[0].hitTargets.length === 0) return;
 
-    const deleteHook = "deleteActiveEffect"
-
     const hpBonus = 5 * (args[0].spellLevel - 1)
 
     args[0].hitTargets.forEach(async target => {
-        const updates = {
-            actor: {
-                'data.attributes.hp.value': target.actor.data.data.attributes.hp.value + hpBonus
-            }
-        }
-
-        await warpgate.mutate(target, updates, {}, { name: `aid` });
-
-            async function removeHealth(effect) {
-                const targetUuid = `Scene.${canvas.scene.id}.Token.${target.uuid}`
-    
-                if (targetUuid !== effect.parent.uuid && effect.sourceName !== 'Aid') return;
-    
-                await warpgate.revert(target, `aid`);
-                Hooks.off(deleteHook, removeHealth)
-            }
-    
-            Hooks.on(deleteHook, removeHealth);
+        await target.actor.update({ "data.attributes.hp.value": target.actor.data.data.attributes.hp.value + hpBonus });
     })
 }
