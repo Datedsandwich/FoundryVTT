60c52a6de8782929b5154742a0bd061aebcaa761
Luke Robinson <robinsonlm@live.co.uk>
2022-01-19 00:03:19 +0000

    Use warpgate for changing token image, truly my favourite module

--- Generic/tokenImageChange.js
+++ Generic/tokenImageChange.js
@@ -4,10 +4,11 @@ const lastArg = args[args.length - 1]
 
 if (args[0] === 'on') {
     const token = await fromUuid(args[args.length - 1].tokenUuid);
-    const updates = { _id: token.id, img: newImage };
-    await token.update(updates);
-} else if(args[0] === 'off') {
+    const updates = { token: { _id: token.id, img: newImage } };
+
+    await warpgate.mutate(token, updates, {}, { name: 'Shapechanged' })
+} else if (args[0] === 'off') {
     const token = await fromUuid(args[args.length - 1].tokenUuid);
-    const updates = { _id: token.id, img: originalImage };
-    await token.update(updates);
+
+    await warpgate.revert(token, 'Shapechanged')
 }
