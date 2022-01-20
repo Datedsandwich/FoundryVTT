e041b672ba3fb5aba0da41e5019026d6458f1863
Luke Robinson <robinsonlm@live.co.uk>
2022-01-20 15:17:28 +0000

    Went back to the ItemMacro approach for swapping token image, it's ultimately simpler in practice than what I was doing before

--- Generic/tokenImageChange.js
+++ Generic/tokenImageChange.js
@@ -1,11 +1,10 @@
-const newImage = args[1]
-const lastArg = args[args.length - 1]
-
 if (args[0] === 'on') {
     const token = await fromUuid(args[args.length - 1].tokenUuid);
-    const updates = { token: { _id: token.id, img: newImage } };
 
-    await warpgate.mutate(token, updates, {}, { name: 'Shapechanged' })
+    const newImage = ''
+
+    const updates = { token: { _id: token.id, img: newImage } };
+    await warpgate.mutate(token, updates, {}, { name: 'Shapechanged' });
 } else if (args[0] === 'off') {
     const token = await fromUuid(args[args.length - 1].tokenUuid);
 
