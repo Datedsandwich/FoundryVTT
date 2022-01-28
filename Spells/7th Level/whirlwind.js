0d9087a277bcfc436c2c87301e89fe71f543be6c
Luke Robinson <robinsonlm@live.co.uk>
2022-01-28 11:49:24 +0000

    With the help of tposney, got the Whirlwind macro to work

--- Spells/7th Level/whirlwind.js
+++ Spells/7th Level/whirlwind.js
@@ -1,5 +1,5 @@
 if (args[0].tag === 'OnUse') {
-    if (args[0].failedSaves.length === 0) return
+    if (args[0].failedSaves.length === 0) return;
 
     const casterToken = await fromUuid(args[0].tokenUuid)
     const caster = casterToken.actor
@@ -20,37 +20,35 @@ if (args[0].tag === 'OnUse') {
 
     const item = new CONFIG.Item.documentClass(itemData, { parent: caster })
 
-    args[0].failedSaves.forEach(async target => {
-        // If they failed the dex save, they also need to make a Strength saving throw
-        const options = { showFullCard: false, createWorkflow: true, versatile: false, configureDialog: false };
-        
-        MidiQOL.completeItemRoll(item, options).then(async save => {
-            console.log(save) // This is almost identical to args[0], but should be the new workflow?
-            const restrained = await game.dfreds.effectInterface.hasEffectApplied('Restrained', target.actor.uuid)
+    const options = { showFullCard: false, createWorkflow: true, versatile: false, configureDialog: false, targetUuids: args[0].failedSaveUuids };
+    const strengthSaves = await MidiQOL.completeItemRoll(item, options);
+    console.log("return is ", strengthSaves)
 
-            if (save.failedSaves.length === 0) {
-                const updates = {
-                    elevation: target.data.elevation + 10
-                }
-    
-                await target.update(updates)
-    
-                if (!restrained) {
-                    game.dfreds.effectInterface.toggleEffect('Restrained', { uuids: [target.actor.uuid] });
-                }
-            } else {
-                console.log('Reducing Elevation')
-    
-                const updates = {
-                    elevation: 0
-                }
-    
-                await target.update(updates)
-    
-                if (restrained) {
-                    game.dfreds.effectInterface.toggleEffect('Restrained', { uuids: [target.actor.uuid] });
-                }
-            }
-        });
+    strengthSaves.failedSaves.forEach(async target => {
+        const updates = {
+            elevation: target.data.elevation + 10
+        }
+
+        await target.data.update(updates)
+
+        const restrained = await game.dfreds.effectInterface.hasEffectApplied('Restrained', target.actor.uuid)
+        if (!restrained) {
+            game.dfreds.effectInterface.toggleEffect('Restrained', { uuids: [target.actor.uuid] });
+        }
+    })
+
+    const failedSaveUuids = [...strengthSaves.failedSaves].map(target => target.document.uuid)
+    const successfulSaves = [...strengthSaves.saves].filter(target => !failedSaveUuids.includes(target.document.uuid))
+
+    successfulSaves.forEach(async target => {
+        const updates = {
+            elevation: 0
+        }
+
+        await target.update(updates)
+
+        if (restrained) {
+            game.dfreds.effectInterface.toggleEffect('Restrained', { uuids: [target.actor.uuid] });
+        }
     })
 }
