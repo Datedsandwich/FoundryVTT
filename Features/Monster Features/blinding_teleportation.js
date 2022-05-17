aae0dee861c4601416eb021a287f84c1139c80d0
Luke Robinson <robinsonlm@live.co.uk>
2022-05-17 19:42:01 +0100

    Adjust the animation on Blinding Teleportation

--- Features/Monster Features/blinding_teleportation.js
+++ Features/Monster Features/blinding_teleportation.js
@@ -4,9 +4,7 @@ if (args[0].tag === "OnUse") {
 
     const centerPosition = canvas.grid.getCenter(template.data.x, template.data.y);
 
-    canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [template.id])
-
-    const offset = sourceToken.data.width / 2 * canvas.grid.size
+    canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [template.id]);
 
     new Sequence().effect()
         .file('modules/jb2a_patreon/Library/Cantrip/Sacred_Flame/SacredFlameSource_01_Regular_Yellow_400x400.webm')
@@ -14,19 +12,19 @@ if (args[0].tag === "OnUse") {
         .scale(1)
         .randomRotation()
         .playbackRate(2)
-        .waitUntilFinished(-500)
-        .animation()
-        .on(sourceToken)
-        .opacity(0)
-        .fadeOut(500)
-        .teleportTo({ x: centerPosition[0], y: centerPosition[1] }, { relativeToCenter: true })
+        .wait(200)
         .effect()
         .file('modules/jb2a_patreon/Library/Cantrip/Sacred_Flame/SacredFlameTarget_01_Regular_Yellow_400x400.webm')
         .atLocation({ x: centerPosition[0], y: centerPosition[1] })
         .scale(2)
         .randomRotation()
         .playbackRate(2)
-        .waitUntilFinished(-500)
+        .waitUntilFinished(-700)
+        .animation()
+        .on(sourceToken)
+        .opacity(0)
+        .fadeOut(500)
+        .teleportTo({ x: centerPosition[0], y: centerPosition[1] }, { relativeToCenter: true })
         .animation()
         .on(sourceToken)
         .opacity(1)
