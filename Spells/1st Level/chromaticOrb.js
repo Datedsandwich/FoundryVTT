f7bacd86e3388e546797ee2221675845104dd125
Luke Robinson <robinsonlm@live.co.uk>
2022-11-25 15:30:47 +0000

    Simplify chromatic orb

--- Spells/1st Level/chromaticOrb.js
+++ Spells/1st Level/chromaticOrb.js
@@ -1,4 +1,4 @@
-if (args[0].tag === 'OnUse') {
+if (args[0].macroPass === 'preDamageRoll') {
     const damageTypeToAnimation = {
         acid: 'jb2a.fire_bolt.green',
         cold: 'jb2a.fire_bolt.blue',
@@ -9,7 +9,6 @@ if (args[0].tag === 'OnUse') {
     }
 
     const casterToken = await fromUuid(args[0].tokenUuid)
-    const caster = casterToken.actor
 
     let damageTypeOptions = {
         buttons: [
@@ -28,30 +27,13 @@ if (args[0].tag === 'OnUse') {
         .effect()
         .file(damageTypeToAnimation[damageType])
         .atLocation(casterToken)
-        .reachTowards(args[0].targets[0])
+        .stretchTo(args[0].targets[0])
         .missed(args[0].hitTargets.length === 0)
         .play()
 
     if (args[0].hitTargets.length === 0) return
 
-    const target = canvas.tokens.get(args[0].hitTargets[0].id)
-    const numDice = args[0].spellLevel + 2
-    const damageRoll = args[0].isCritical
-        ? await new Roll(`${numDice}d8 + ${numDice * 8}`).evaluate({
-              async: true,
-          }) // This is how crits are handled in my game
-        : await new Roll(`${numDice}d8`).evaluate({ async: true })
+    const theItem = await fromUuid(args[0].uuid)
 
-    new MidiQOL.DamageOnlyWorkflow(
-        caster,
-        casterToken,
-        damageRoll.total,
-        damageType,
-        [target],
-        damageRoll,
-        {
-            flavor: `(${CONFIG.DND5E.damageTypes[damageType]})`,
-            itemCardId: args[0].itemCardId,
-        }
-    )
+    theItem.system.damage.parts[0][1] = damageType
 }
